0755356b8d22d0270455c4a276a68fad5b38a65c
LucasSales <lucasg17.lima@gmaill.com>
2019-06-22 12:09:26 -0300

    Teste esquema

--- ESQUEMA_PROJETO_TESTE.sql
+++ ESQUEMA_PROJETO_TESTE.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA `teste_projeto` ;
+CREATE SCHEMA `teste_projeto`:
 USE `teste_projeto`;
 
 CREATE TABLE `teste_projeto`.`funcionario` (
@@ -7,6 +7,32 @@ CREATE TABLE `teste_projeto`.`funcionario` (
   `dataNascimento` DATE NULL,
   `email` VARCHAR(60) NULL,
   PRIMARY KEY (`id`));
+
+CREATE TABLE `teste_projeto`.`Regiao` (
+    `cod_regiao` INT NOT NULL AUTO_INCREMENT,
+    `logradouro` VARCHAR(12),
+    `numero` VARCHAR(12),
+    `complemento` VARCHAR(12),
+    `bairro` VARCHAR(12),
+    `cidade` VARCHAR(12),
+    `estado` VARCHAR(12),
+    `pais` VARCHAR(12),
+    PRIMARY KEY (`cod_regiao`));
+
+CREATE TABLE `teste_projeto`.`bandeira`(
+    nome_bandeira VARCHAR(32) NOT NULL,
+    url,
+    PRIMARY KEY (`nome_bandeira`));
+
+CREATE TABLE `teste_projeto`.`posto`(
+    cnpj_posto VARCHAR(12),
+    razao_social VARCHAR(12),
+    nome_fantasia VARCHAR(12),
+    telefone VARCHAR(12),
+    PRIMARY KEY(`cnpj_posto`));
+
+ALTER TABLE `posto` ADD CONSTRAINT `fk_cod_regiao` FOREIGN KEY ( `cod_regiao` ) REFERENCES `Regiao` ( `cod_regiao` );
+ALTER TABLE `posto` ADD CONSTRAINT `fk_nome_bandeira` FOREIGN KEY ( `nome_bandeira` ) REFERENCES `bandeira` ( `nome_bandeira` );
 
 INSERT INTO `teste_projeto`.`funcionario` (`nome`, `dataNascimento`, `email`) VALUES
 ('João José', '1988-12-12', 'joao_jose@gmail.com'),
