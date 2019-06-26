d187ade6b48f3d899e03f9c695035ad123a57099
LucasSales <lucasg17.lima@gmaill.com>
2019-06-26 01:14:41 -0300

    interface melhorada

--- interface/gui.py
+++ interface/gui.py
@@ -8,8 +8,9 @@ class Gui:
         self.BUTTON_PATH = '.'
         self.button_names_main = ('Abastecer', 'Registro', 'Close')
         self.button_names_abs = ('Voltar','Calcular','Finalizar')
-        self.button_names_regis = ()
+        self.button_names_regis = ('Ok', 'Cancelar')
         self.window = None
+        self.window_popup = None
 
     # toolbar_buttons
     def configToobarBtn(self, lista_btn):
@@ -27,6 +28,7 @@ class Gui:
         toolbar_buttons_init    = self.configToobarBtn(self.button_names_main)
         toolbar_buttons_abs     = self.configToobarBtn(self.button_names_abs)
         toolbar_buttons_regis   = self.configToobarBtn(self.button_names_regis)
+        toolbar_buttons_popup   = self.configToobarBtn(self.button_names_regis)
 
 # Layout
         layout = [
@@ -35,16 +37,16 @@ class Gui:
                  ]
 
         # chamar do banco o combustivel e preço
-        preco = str(3.84)
-        combustivel = 'Gasolina'
+        preco = None
+        combustivell = None
 
         abastecerLayout = [
                             [sg.Text("Escolha uma das opções abaixo:")],
                             [sg.Text('Matricula:'),sg.InputText('', key='Matricula')],
                             [sg.Text('Veiculo:'), sg.InputText('', key='Veiculo')],
                             [sg.Text('Cliente:'), sg.InputText('', key='Cliente')],
-                            [sg.Text('Combustivel:'), sg.Text(text=combustivel, key='Combustivel')],
-                            [sg.Text('Preço:'), sg.Text(text=preco, key='Preco')],
+                            [sg.Text('Combustivel:'), sg.Text('', key='Combustivel')],
+                            [sg.Text('Preço:'), sg.Text('', key='Preco')],
                             [sg.Text('Quantidade:'), sg.InputText('', key='Quantidade')],
                             [sg.Text('Subtotal:'), sg.Text('', key='SubTotal')],
                             [sg.Frame('', toolbar_buttons_abs)],
@@ -54,35 +56,61 @@ class Gui:
                             [sg.Text("Cadastre conforme é pedido:")],
                             [sg.Frame('', toolbar_buttons_regis)]
                          ]
+# nesta tupla ficará todos os combustiveis + preços
+        names = [['gasolina', '4.75'], ['alcool', '3.28'], ['diesel', '4.55'], ['gas', '2.10'], ['ração', '7.38']]
+        tableLayout = [
+            [sg.Table(values=names,
+                      enable_events=False,
+                      display_row_numbers=True,
+                      headings=['Combustivel', 'Preço'],
+                      key='_table_',
+                      select_mode=True)],
+            [sg.Frame('', toolbar_buttons_popup)]
+        ]
 
 # Windows
         self.window = sg.Window('Posto LAR', location=(600, 450), size=(400, 400)).Layout(layout)
 
 # Read Window
+        x = 0
         while True:
 
             button, value = self.window.Read()
             try:
+                print()
 
-                print("")
-            except:
-                print("here")
             finally:
 
                 if button == 'Registro':
                     self.window.Close()
                     self.window = sg.Window('Posto LAR', location=(600, 450), size=(400, 400)).Layout(registroLayout)
 
-                elif button == 'Abastecer':
+                if x == 1:
+                    self.window.Element('Combustivel').Update(combustivell)
+                    self.window.Element('Preco').Update(preco)
+                    x = 0
+
+                if button == 'Abastecer':
                     self.window.Close()
+
+                    self.window_popup= sg.Window("Escolha o combustivel",location=(600, 450), size=(400, 400)).Layout(tableLayout)
+                    button_popup,value_popup = self.window_popup.Read()
+
+                    if button_popup == 'Ok':
+                        combustivel = names[value_popup['_table_'][0]]
+                        preco = combustivel[1]
+                        combustivell = combustivel[0]
+                        self.window_popup.Close()
+
                     self.window = sg.Window('Posto LAR', location=(600, 450), size=(400, 400)).Layout(abastecerLayout)
+                    x = 1
 
                 elif button == 'Voltar':
                     self.window.Close()
                     self.window = sg.Window('Posto LAR', location=(600, 450), size=(400, 400)).Layout(layout)
 
                 elif button == 'Calcular':
-                    novoPreco = float(value['Quantidade']) * float(preco)
+                    novoPreco = float(value['Quantidade']) * float(combustivel[1])
                     self.window.Element('SubTotal').Update(novoPreco)
 
 
@@ -97,6 +125,7 @@ class Gui:
                 else:
                     self.window.Close()
                     break
+
 
 teste = Gui()
 teste.show()
